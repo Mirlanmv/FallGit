6754a122e059bb14415bf24b7ea0272a35e0b55a
MIrlan <mirlanks@mail.ru>
2024-05-30 11:11:14 +0600

    c1 commit-3

--- src/c/C1.java
+++ src/c/C1.java
@@ -4,4 +4,8 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("HEllo world");
     }
+
+    public static void sayHello() {
+        System.out.println("HEllO");
+    }
 }
